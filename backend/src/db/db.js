d8b20e7efc3e147c82474e6c5249e42eb2eff8fb
nikhilarote07 <37741518+nikhilarote07@users.noreply.github.com>
2020-05-09 15:56:50 +0530

    added new changes

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose.connect("mongodb://localhost:27017/user", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: true
+    useFindAndModify: true,
+    useUnifiedTopology: true
 })
